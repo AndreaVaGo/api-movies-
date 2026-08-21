20f16ab250b414ac45d1e66bcd7ae629226d4f4e
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-08-21 11:26:35 +0200

    Add JPA repositories

---
+++ src/main/java/dev/andrea/api_movies/genero/GeneroRepository.java
@@ -0,0 +1,6 @@
+package dev.andrea.api_movies.genero;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneroRepository extends JpaRepository<GeneroEntity, Long> {
+}
