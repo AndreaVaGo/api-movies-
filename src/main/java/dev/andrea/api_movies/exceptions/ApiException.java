b35a9a078a7df23c2e9b32c5be9ce1b94a2b12af
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-08-24 13:06:26 +0200

    Add validation dependency and base ApiException

---
+++ src/main/java/dev/andrea/api_movies/exceptions/ApiException.java
@@ -0,0 +1,12 @@
+package dev.andrea.api_movies.exceptions;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
